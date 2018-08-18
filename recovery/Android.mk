5bf47be585619fc5c9fa4b9f6f449d90081e58fa
martinusbe <martinusbe@gmail.com>
2018-08-18 18:07:39 +0200

    msm8998: updater: use direct paths to recovery

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,9 +7,9 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(call project-path-for,recovery)/edify/include \
-    $(call project-path-for,recovery)/otautil/include \
-    $(call project-path-for,recovery)/updater/include
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
